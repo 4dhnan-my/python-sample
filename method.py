29c8f6bc725403ccec0e396a485c7b2c03ce3829
Fida <fidafiroz1002@gmail.com>
2026-05-18 14:18:11 +0530

    method

---
+++ method.py
@@ -0,0 +1,21 @@
+#class person:
+ #   def __init__(self,name):
+  #      self.name = name
+   #     
+    #def greet(self):
+     #   print("Hello, my name is " + self.name)
+
+
+#p1=person(input("Enter your name " ))
+#p1.greet()
+
+class person:
+    def __init__(self,name):
+       self.name = name
+        
+    def greet(self):
+        print("Hello, my name is " + self.name)
+
+
+p2=person("bilal")
+p2.greet()
